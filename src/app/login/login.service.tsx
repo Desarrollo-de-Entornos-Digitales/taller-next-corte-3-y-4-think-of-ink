036c6408c0643ca5b6b857528090d85bc48c77c7
laura sofia guerrero <lauraante31@gmail.com>
2026-04-27 00:30:01 -0500

    avances de fronted

--- src/app/login/login.service.tsx
+++ src/app/login/login.service.tsx
@@ -1,10 +1,8 @@
-import axios from 'axios';
-
-const API_URL = 'http://localhost:3000/auth'; // ajusta si tu puerto cambia
+import api from '@/lib/axios';
 
 export const loginUser = async (email: string, password: string) => {
   try {
-    const response = await axios.post(`${API_URL}/login`, {
+    const response = await api.post('/auth/login', {
       email,
       password,
     });
